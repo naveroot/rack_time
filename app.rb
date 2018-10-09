e1d0c9f1ec159f2301ac3ff1c20a44865a2d7f78
naveroot <naveroot@gmail.com>
2018-10-09 16:40:01 +0300

    First try

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
 class Application
+  AVAILABLE_TIME_FORMATS = %w[year month day hour minute second].freeze
+  TIME_HASH = {
+      year: 'year',
+      month: 'month',
+      day: 'day',
+      hour: 'hour',
+      minute: 'min',
+      second: 'sec'
+  }.freeze
 
   def call(env)
-    @request = Rack::Request.new(env)
-    if @request.path_info == '/time'
-      [status, headers, body]
+    request = Rack::Request.new(env)
+    params = request.params
+    return [404, headers, ['Not found']] if valid_url?(request)
+    format_params = params['format'].split(',')
+    if formats_valid?(format_params)
+      [500, headers, [time_output(format_params)]]
     else
-      [404, headers, '404']
+      [400, headers, ["Unknown time format [#{(format_params - AVAILABLE_TIME_FORMATS).join(',')}]"]]
     end
   end
 
@@ -21,5 +33,21 @@ class Application
 
   def body
     ["Greetings!!\n"]
+  end
+
+  def valid_url?(request)
+    request.params['format'].nil? || request.params['format'].empty? || request.path_info != '/time'
+  end
+
+  def formats_valid?(format_params)
+    (format_params - AVAILABLE_TIME_FORMATS).empty?
+  end
+
+  def time_output(format_params)
+    output = []
+    format_params.each do |param|
+      output << Time.now.send(TIME_HASH[param.to_sym])
+    end
+    p output.join('-')
   end
 end
--- config.ru
+++ config.ru
@@ -1,11 +1,9 @@
 require_relative 'middleware/runtime'
 require_relative 'middleware/logger'
-require_relative 'middleware/time_format'
 require_relative 'app'
 
 use Rack::Reloader
 use Runtime
 use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
-use TimeFormat
 
 run Application.new
